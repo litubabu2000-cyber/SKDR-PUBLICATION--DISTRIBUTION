3f7d936d5641e9f095c22dc58aef0cea0dad606e
Rashmiranjan Palei <litubabu2000@gmail.com>
2025-11-05 14:55:36 +0000

    Try fixing this error: `Console Error: A param property was accessed dir

--- src/app/for-teachers/kvs/[class]/page.tsx
+++ src/app/for-teachers/kvs/[class]/page.tsx
@@ -1,6 +1,4 @@
 
-'use client';
-
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { BookOpen, FlaskConical, BarChart } from "lucide-react";
 import { notFound } from "next/navigation";
