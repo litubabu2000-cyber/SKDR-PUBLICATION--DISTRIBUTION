47c26c5487a81097e6550f975d31f5d3fa7d4cbf
Rashmiranjan Palei <litubabu2000@gmail.com>
2025-11-11 04:48:08 +0000

    Try fixing this error: `Runtime Error: ReferenceError: Cannot access 'Mi

--- src/app/mcq-practice/aptitude/[topic]/page.tsx
+++ src/app/mcq-practice/aptitude/[topic]/page.tsx
@@ -4,6 +4,10 @@ import { Train, Building, Banknote, PenTool } from "lucide-react";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
+const Mix = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M19.35 10.04A7.49 7.49 0 0 0 12 4C9.11 4 6.6 5.64 5.35 8.04A5.994 5.994 0 0 0 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5c0-2.64-2.05-4.78-4.65-4.96zM19 18H6c-2.21 0-4-1.79-4-4c0-2.05 1.53-3.76 3.56-3.97l1.07-.11l.5-1.05A5.495 5.495 0 0 1 12 6c2.61 0 4.83 1.8 5.4 4.22l.28 1.15l1.16.1C20.97 11.54 22 12.68 22 14c0 1.65-1.35 3-3 3z"/><path fill="currentColor" d="m14.24 13.76l-1.41 1.41l-1.07-1.06l-1.06 1.06l-1.41-1.41l1.06-1.06l-1.06-1.06l1.41-1.41l1.06 1.06l1.07-1.06l1.41 1.41l-1.06 1.06z"/></svg>
+)
+
 const examCategories = [
   { name: "Railway", icon: Train, href: "railway" },
   { name: "SSC", icon: Building, href: "ssc" },
@@ -45,7 +49,3 @@ export default function AptitudeTopicPage({ params }: { params: { topic: string 
     </div>
   );
 }
-
-const Mix = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M19.35 10.04A7.49 7.49 0 0 0 12 4C9.11 4 6.6 5.64 5.35 8.04A5.994 5.994 0 0 0 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5c0-2.64-2.05-4.78-4.65-4.96zM19 18H6c-2.21 0-4-1.79-4-4c0-2.05 1.53-3.76 3.56-3.97l1.07-.11l.5-1.05A5.495 5.495 0 0 1 12 6c2.61 0 4.83 1.8 5.4 4.22l.28 1.15l1.16.1C20.97 11.54 22 12.68 22 14c0 1.65-1.35 3-3 3z"/><path fill="currentColor" d="m14.24 13.76l-1.41 1.41l-1.07-1.06l-1.06 1.06l-1.41-1.41l1.06-1.06l-1.06-1.06l1.41-1.41l1.06 1.06l1.07-1.06l1.41 1.41l-1.06 1.06z"/></svg>
-)
